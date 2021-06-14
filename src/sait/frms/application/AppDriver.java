4e68edd93a26e023de5f2453bf4f31c9433c7ab2
liamandaidan <liammclaughlinis@gmail.com>
2021-06-13 22:35:50 -0600

    Change to PR

--- src/sait/frms/application/AppDriver.java
+++ src/sait/frms/application/AppDriver.java
@@ -14,7 +14,6 @@ public class AppDriver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");//Just a Test
 		MainWindow mainWindow = new MainWindow();
 		mainWindow.display();
 	}
