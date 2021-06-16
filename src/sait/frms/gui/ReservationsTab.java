bc857ab09e1c7f1c0cbb421322134a1cd57342c0
liamandaidan <liammclaughlinis@gmail.com>
2021-06-16 07:09:00 -0600

    Updated the layout to look about right

--- src/sait/frms/gui/ReservationsTab.java
+++ src/sait/frms/gui/ReservationsTab.java
@@ -7,40 +7,86 @@ import sait.frms.manager.ReservationManager;
 import sait.frms.problemdomain.Reservation;
 
 /**
- * Holds the components for the reservations tab.
- * todo
+ * Holds the components for the reservations tab. todo
  */
 public class ReservationsTab extends TabBase {
 	/**
 	 * Instance of reservation manager.
 	 */
 	private ReservationManager reservationManager;
-	
+
 	private JList<Reservation> reservationsList;
-	
+
 	/**
 	 * Creates the components for reservations tab.
 	 */
 	public ReservationsTab(ReservationManager reservationManager) {
 		this.reservationManager = reservationManager;
-		panel.setLayout(new BorderLayout());
-		
+		panel.setLayout(new BorderLayout(10, 10));
+		panel.setPreferredSize(new Dimension(700, 500));
 		JPanel northPanel = createNorthPanel();
 		panel.add(northPanel, BorderLayout.NORTH);
+
+		JPanel centerPanel = createCenterPanel();
+		panel.add(centerPanel, BorderLayout.CENTER);
+
+		JPanel eastPanel = createEastPanel();
+		panel.add(eastPanel, BorderLayout.EAST);
+
+		JPanel southPanel = createSouthPanel();
+		panel.add(southPanel, BorderLayout.SOUTH);
 	}
-	
+
+	/**
+	 * Creates the center panel.
+	 * 
+	 * @return JPanel that goes in the center.
+	 */
+	private JPanel createCenterPanel() {
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(100, 50));
+		panel.setBackground(Color.CYAN);
+		return panel;
+	}
+
+	/**
+	 * Creates the east panel.
+	 * 
+	 * @return JPanel that goes in east.
+	 */
+	private JPanel createEastPanel() {
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(200, 100));
+		panel.setBackground(Color.YELLOW);
+		return panel;
+	}
+
+	/**
+	 * Creates the south panel.
+	 * 
+	 * @return JPanel that goes in south.
+	 */
+	private JPanel createSouthPanel() {
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(100, 120));
+		panel.setBackground(Color.GREEN);
+		return panel;
+	}
+
 	/**
 	 * Creates the north panel.
+	 * 
 	 * @return JPanel that goes in north.
 	 */
-	private JPanel createNorthPanel() 
-	{
+	private JPanel createNorthPanel() {
 		JPanel panel = new JPanel();
-		
+
 		JLabel title = new JLabel("Reservations", SwingConstants.CENTER);
 		title.setFont(new Font("serif", Font.PLAIN, 29));
+		panel.setPreferredSize(new Dimension(100, 60));
+		panel.setBackground(Color.RED);
 		panel.add(title);
-		
+
 		return panel;
 	}
 }
