8e698cc37679dd3ec342aa18744647162f6e483d
rawbz <rmjbalanag@gmail.com>
2021-06-29 10:52:21 -0600

    Update ReservationManager.java

--- src/sait/frms/manager/ReservationManager.java
+++ src/sait/frms/manager/ReservationManager.java
@@ -14,7 +14,7 @@ import sait.frms.problemdomain.*;
  *          TODO Refer to GITHub online for issues to be completed.
  */
 
-public class ReservationManager extends Reservation {
+public class ReservationManager {
 
 	private ArrayList<Reservation> reservations = new ArrayList<>();
 
@@ -22,8 +22,7 @@ public class ReservationManager extends Reservation {
 	 * 
 	 */
 	public ReservationManager() {
-		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
@@ -31,6 +30,7 @@ public class ReservationManager extends Reservation {
 	 */
 	public Reservation makeReservation(Flight flight, String name, String citizenship) {
 		Reservation newReservation = new Reservation();
+
 		return newReservation;
 
 	}
@@ -39,15 +39,38 @@ public class ReservationManager extends Reservation {
 	 * @return the list of reservations
 	 */
 	public ArrayList<Reservation> findReservations(String code, String airline, String name) {
-		return reservations;
+		ArrayList<Reservation> foundReservation = new ArrayList<>();
+
+		for (int i = 0; i < reservations.size(); i++) {
+			if (reservations.get(i).getCode().equals(code) && reservations.get(i).getAirline().equals(airline)
+					&& reservations.get(i).getName().equals(name)) {
+				foundReservation.add(reservations.get(i));
+			}
+			if (foundReservation.size() == 0) {
+				System.out.println("No reservation found.");
+			}
+		}
+
+		return foundReservation;
+
 	}
 
 	/**
 	 * @return the reservation using the code
 	 */
 	public Reservation findReservationByCode(String code) {
-		Reservation reservationByCode = new Reservation();
-		return reservationByCode;
+
+		for (int i = 0; i < reservations.size(); i++) {
+			if (reservations.get(i).getCode().equals(code)) {
+				return reservations.get(i);
+			} else {
+				System.out.println("No reservation found.");
+				return null;
+			}
+
+		}
+
+		return null;
 
 	}
 
@@ -61,8 +84,8 @@ public class ReservationManager extends Reservation {
 	/**
 	 * @return the number of available seats
 	 */
-	private int getAvailableSeats() {
-		return 0;
+	private int getAvailableSeats(Flight flight) {
+		return flight.getSeats();
 	}
 
 	/**
@@ -73,18 +96,21 @@ public class ReservationManager extends Reservation {
 	 * 
 	 * @return a generated confirmation code
 	 */
-	private String generateReservationCode(Flight flight) throws InvalidFlightCodeException{
-		//Potentially Throws error of Having the wrong flight code.
+	private String generateReservationCode(Flight flight) throws InvalidFlightCodeException {
+		// Potentially Throws error of Having the wrong flight code.
 		String code = "";
 		char fromLetter = flight.getFrom().charAt(0);
 		char toLetter = flight.getTo().charAt(0);
-			if (fromLetter == toLetter && fromLetter == 'Y' && Character.isLetter(fromLetter)&&Character.isLetter(toLetter)) //if the first letter at From = first Letter of To AND if one letter starts with Y
-				code += "D";
-			else if(Character.isLetter(fromLetter)&&Character.isLetter(toLetter))//Check to see if both are Letter inputs.
-				code += "I";
-			else
-				throw new InvalidFlightCodeException(flight.getFrom(),flight.getTo());
-			code += (int)(Math.random()*9000+1000); //1000-9999
+		if (fromLetter == toLetter && fromLetter == 'Y' && Character.isLetter(fromLetter)
+				&& Character.isLetter(toLetter)) // if the first letter at From = first Letter of To AND if one letter
+													// starts with Y
+			code += "D";
+		else if (Character.isLetter(fromLetter) && Character.isLetter(toLetter))// Check to see if both are Letter
+																				// inputs.
+			code += "I";
+		else
+			throw new InvalidFlightCodeException(flight.getFrom(), flight.getTo());
+		code += (int) (Math.random() * 9000 + 1000); // 1000-9999
 
 		return code;
 
