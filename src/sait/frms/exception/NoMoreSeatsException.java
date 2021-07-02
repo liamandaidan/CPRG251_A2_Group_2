0520e00733f40b4a96c5b70f9c1150aa5302b44f
liamandaidan <liammclaughlinis@gmail.com>
2021-07-02 10:54:06 -0600

    Update NoMoreSeatsException.java

--- src/sait/frms/exception/NoMoreSeatsException.java
+++ src/sait/frms/exception/NoMoreSeatsException.java
@@ -1,5 +1,11 @@
 package sait.frms.exception;
 
+/**
+ * This exception is thrown when there are no more seats available.
+ * @author Robyn Balanag
+ *
+ */
+
 public class NoMoreSeatsException extends Exception {
 
 	public NoMoreSeatsException() {
